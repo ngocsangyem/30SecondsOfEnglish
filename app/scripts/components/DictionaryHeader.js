a9b1c260e45efd2d79f264316e528f241a09faf7
sanggnguyenn <sang.nguyenngoc@gforces.auto>
2021-02-09 11:31:39 +0700

    update data

---
+++ app/scripts/components/DictionaryHeader.js
@@ -0,0 +1,14 @@
+export default class DictionaryHeader {
+	constructor(el) {
+		this.el = el;
+	}
+
+	templates() {
+		return `
+		<div class="dictionary-header">
+			<h1 class="dictionary-name">go
+			</h1>
+		</div>
+		`;
+	}
+}
